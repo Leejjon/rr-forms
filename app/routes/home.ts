d34bf751b27284517056ea9ac29d35747e0dcc95
Leejjon <leejjon@gmail.com>
2026-08-18 00:22:41 +0200

    Most basic unsafe form

---
+++ app/routes/home.ts
@@ -0,0 +1,16 @@
+import type {Route} from "./+types/home";
+
+export async function loader(args: Route.LoaderArgs) {
+    const commentForm = "<form action='/api/comments' method='POST'>" +
+        "<label>Name:</label><br />" +
+        "<input name='name' /><br />" +
+        "<label>Message:</label><br />" +
+        "<textarea name='message'></textarea><br />" +
+        "<br />" +
+        "<button type='submit'>Submit</button>" +
+        "</form>";
+    return new Response(`<html>${commentForm}</html>`, {
+        headers: {"Content-Type": "text/html"},
+        status: 200
+    });
+}
