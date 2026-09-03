39ff14c70ed691a860ae222e10787665a98648d1
Leejjon <leejjon@gmail.com>
2026-09-04 00:57:28 +0200

    Turned autocomplete off.

--- app/routes/home.ts
+++ app/routes/home.ts
@@ -4,6 +4,27 @@ import {randomUUID} from "node:crypto";
 import type {Comment} from "~/common/comments";
 import {redirect} from "react-router";
 
+function createCommentsElements() {
+    return `<div>${getComments().map((comment) => {
+        return `<div key="${comment.id}"><b>${comment.name}:</b> ${comment.message}` +
+            `<br /><i>Posted at ${comment.timestamp}</i></div>`
+    }).join('<br/>')}</div>`;
+}
+
+function createFormElement(nameError?: string) {
+    const nameErrorLabel = `<label style="color: red; margin-left: 1em;">${nameError}</label>`;
+    return "<form action='/' method='POST'>" +
+        `${createCommentsElements()}` +
+        "<br />" +
+        "<label>Name:</label><br />" +
+        `<input name='name' autocomplete='off' />${nameError !== undefined ? nameErrorLabel : ''}<br />` +
+        "<label>Message:</label><br />" +
+        "<textarea name='message'></textarea><br />" +
+        "<br />" +
+        "<button type='submit'>Submit</button>" +
+        "</form>";
+}
+
 export async function action({request}: Route.ActionArgs) {
     const formData = await request.formData();
     const name = formData.get("name");
@@ -17,29 +38,15 @@ export async function action({request}: Route.ActionArgs) {
         addComment({id: randomUUID(), timestamp: new Date(), name, message} as Comment);
         return redirect("/");
     } else {
-        return new Response("Invalid name", {
-            status: 400
+        return new Response(`<html>${createFormElement('You entered an invalid name.')}</html>`, {
+            headers: {"Content-Type": "text/html"},
+            status: 200
         });
     }
 }
 
 export async function loader(args: Route.LoaderArgs) {
-    const commentsElement = `<div>${getComments().map((comment) => {
-        return `<div key="${comment.id}"><b>${comment.name}:</b> ${comment.message}` +
-            `<br /><i>Posted at ${comment.timestamp}</i></div>`
-    }).join('<br/>')}</div>`;
-
-    const commentForm = "<form action='/' method='POST'>" +
-        `${commentsElement}` +
-        "<br />" +
-        "<label>Name:</label><br />" +
-        "<input name='name' /><br />" +
-        "<label>Message:</label><br />" +
-        "<textarea name='message'></textarea><br />" +
-        "<br />" +
-        "<button type='submit'>Submit</button>" +
-        "</form>";
-    return new Response(`<html>${commentForm}</html>`, {
+    return new Response(`<html>${createFormElement()}</html>`, {
         headers: {"Content-Type": "text/html"},
         status: 200
     });
