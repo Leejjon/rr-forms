438b6d28e58a84142223cde63c88d08451969dfb
Leejjon <leejjon@gmail.com>
2026-09-12 02:43:59 +0200

    Added a placeholder to prevent the error label to show when the user hasn't typed anything yet.

--- app/routes/home.ts
+++ app/routes/home.ts
@@ -11,13 +11,13 @@ function createCommentsElements() {
     }).join('<br/>')}</div>`;
 }
 
-function createFormElement(nameError?: string) {
-    const nameErrorLabel = `<label style="color: red; margin-left: 1em;">${nameError}</label>`;
+function createFormElement() {
+    const nameErrorLabel = `<label class="error">You entered an invalid name.</label>`;
     return "<form action='/' method='POST'>" +
         `${createCommentsElements()}` +
         "<br />" +
         "<label>Name:</label><br />" +
-        `<input name='name' autocomplete='off' />${nameError !== undefined ? nameErrorLabel : ''}<br />` +
+        `<input name='name' required minlength='1' maxlength='20' pattern='^[A-Za-z0-9]+(?:[ _\\-][A-Za-z0-9]+)*$' placeholder='' />${nameErrorLabel}<br />` +
         "<label>Message:</label><br />" +
         "<textarea name='message'></textarea><br />" +
         "<br />" +
@@ -32,7 +32,7 @@ export async function action({request}: Route.ActionArgs) {
 
     function nameIsValid(): boolean {
         if (name) {
-            return name.toString().length > 0 && name.toString().length < 20 &&
+            return name.toString().length > 0 && name.toString().length <= 20 &&
                 name.toString().match(/^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/) !== null;
         }
         return false;
@@ -42,15 +42,28 @@ export async function action({request}: Route.ActionArgs) {
         addComment({id: randomUUID(), timestamp: new Date(), name, message} as Comment);
         return redirect("/");
     } else {
-        return new Response(`<html>${createFormElement('You entered an invalid name.')}</html>`, {
-            headers: {"Content-Type": "text/html"},
-            status: 200
+        return new Response(`Invalid request`, {
+            status: 400
         });
     }
 }
 
+const headAndStyleElement = "<head>" +
+    "<style>" +
+    "input:not(:placeholder-shown):invalid {" +
+        "border: 2px solid red;" +
+        "background-color: #ffecec;" +
+    "}"+
+    "input:not(:placeholder-shown):invalid + .error {" +
+        "display: inline;" +
+        "color: red;" +
+    "}" +
+    ".error { display: none; color: red; margin-left: 1em; }" +
+    "</style>" +
+    "</head>";
+
 export async function loader(args: Route.LoaderArgs) {
-    return new Response(`<html>${createFormElement()}</html>`, {
+    return new Response(`<html>${headAndStyleElement}${createFormElement()}</html>`, {
         headers: {"Content-Type": "text/html"},
         status: 200
     });
